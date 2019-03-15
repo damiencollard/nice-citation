3cf0180833407ef3d551b4969b3462458e6acb4f
Damien Collard <damien.collard@laposte.net>
2019-03-15 16:07:19 +0100

    Add keywords, package requirements, license

--- nice-citation.el
+++ nice-citation.el
@@ -1,5 +1,26 @@
 ;;; nice-citation.el --- Nicer, colored citation marks.
 
+;; Copyright (c) 2019 Damien Collard
+
+;; Author: Damien Collard <damien.collard@laposte.net>
+;; URL:
+;; Version: 0.0.1
+;; Keywords: gnus mail convenience
+;; Package-Requires: ((emacs "24.3") (gnus "5.13"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; In Gnus' article mode, replace the traditional chevron citation marks (`>`)
